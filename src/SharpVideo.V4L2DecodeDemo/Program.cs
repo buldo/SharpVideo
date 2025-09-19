4632b428743970165d44be9390b160c77e51b21e
Roman Buldygin <froooks@gmail.com>
2025-09-20 00:21:26 +0300

    New project

---
+++ src/SharpVideo.V4L2DecodeDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpVideo.V4L2DecodeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
