9a367db868f72d142f97ec9a897b4b38c17444df
Roman Buldygin <froooks@gmail.com>
2025-09-21 00:03:01 +0300

    save

---
+++ src/SharpVideo/V4L2/V4L2Device.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpVideo.V4L2;
+
+public class V4L2Device
+{
+    public static List<V4L2Device> FindH264Decoders()
+    {
+
+    }
+}
---
+++ src/SharpVideo/V4L2/V4L2DeviceManager.cs
@@ -0,0 +1,35 @@
+﻿namespace SharpVideo.V4L2;
+
+public static class V4L2DeviceManager
+{
+    private static string[] GetVideoDevices()
+    {
+        return Directory.EnumerateFileSystemEntries("/dev", "video*")
+            .Where(CheckForSymlink)
+            .ToArray();
+
+        bool CheckForSymlink(string path)
+        {
+            var fileInfo = new FileInfo(path);
+
+            // This is not symlink
+            if (!fileInfo.Attributes.HasFlag(FileAttributes.ReparsePoint))
+            {
+                return true;
+            }
+
+            var target = fileInfo.ResolveLinkTarget(true);
+            if (target == null)
+            {
+                return true;
+            }
+
+            return (fileInfo.Attributes) == 0;
+        }
+    }
+
+    public static List<string> FindByOutputPixelFormat()
+    {
+
+    }
+}
