9042ad06fa2a79cacaf236f6319da8a9c431255f
Roman Buldygin <froooks@gmail.com>
2025-09-28 22:11:12 +0300

    RefPicListModificationState

---
+++ src/SharpVideo/H264/RefPicListModificationState.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpVideo.H264;
+
+/// <summary>
+/// The parsed state of the RefPicListModification.
+/// </summary>
+public class RefPicListModificationState
+{
+
+    // input parameters
+    public uint32_t slice_type = 0;
+
+    // contents
+    public uint32_t ref_pic_list_modification_flag_l0 = 0;
+    public List<uint32_t> modification_of_pic_nums_idc;
+    public List<uint32_t> abs_diff_pic_num_minus1;
+    public List<uint32_t> long_term_pic_num;
+    public uint32_t ref_pic_list_modification_flag_l1 = 0;
+}
