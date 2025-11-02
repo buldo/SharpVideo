72cf996f018a9a2790c234330a8b716c084794db
Roman Buldygin <froooks@gmail.com>
2025-11-02 21:15:43 +0300

    Avalonia demo app

---
+++ src/SharpVideo.AvaloniaMpDemo/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SharpVideo.AvaloniaMpDemo;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/SharpVideo.AvaloniaMpDemo/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SharpVideo.AvaloniaMpDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
