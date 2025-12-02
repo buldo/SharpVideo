2b3bb15bd634be91498fcab9523ef203024d6837
Roman Buldygin <froooks@gmail.com>
2025-12-03 00:03:26 +0300

    new ffmpeg project

---
+++ src/Examples/SharpVideo.FfmpegDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpVideo.FfmpegDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
