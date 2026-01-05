7ea52437f2cdb16af1de73ef7659932cc8519096
Roman Buldygin <froooks@gmail.com>
2026-01-06 00:01:54 +0300

    maybe it helps

--- src/SharpVideo.Decoding/UniversalEncodedBuffer.cs
+++ src/SharpVideo.Decoding/UniversalEncodedBuffer.cs
@@ -13,7 +13,21 @@ public abstract class UniversalEncodedBuffer
 
     protected int GetPayloadStart(ReadOnlySpan<byte> nalu)
     {
-        ReadOnlySpan<byte> fourByteStart = stackalloc byte[] { 0x00, 0x00, 0x00, 0x01 };
-        return nalu.Length >= 4 && nalu.Slice(0, 4).SequenceEqual(fourByteStart) ? 4 : 3;
+        // Check for 4-byte start code: 0x00 0x00 0x00 0x01
+        if (nalu.Length >= 4 &&
+            nalu[0] == 0x00 && nalu[1] == 0x00 && nalu[2] == 0x00 && nalu[3] == 0x01)
+        {
+            return 4;
+        }
+
+        // Check for 3-byte start code: 0x00 0x00 0x01
+        if (nalu.Length >= 3 &&
+            nalu[0] == 0x00 && nalu[1] == 0x00 && nalu[2] == 0x01)
+        {
+            return 3;
+        }
+
+        // No start code found - data starts at beginning
+        return 0;
     }
 }
